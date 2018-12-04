1ed667a25822765cc6b5d84c4757db46f7007364
Jayden Kerr <jayden@jaydenkerr.net>
2018-12-04 23:30:05 +1030

    Completed Day 1.2

---
+++ Day 1/Day 1.2.py
@@ -0,0 +1,24 @@
+# Advent of Code 2018
+# Jayden Kerr
+# Day 1
+# Puzzle 2
+
+# Might come back to this - I need to save the session cookie once logged in order to cURL the contents of the page.
+#import requests
+
+#inputRequest = requests.get('https://adventofcode.com/2018/day/1/input')
+#print(inputRequest.text)
+
+# Import itertools to iterate over the input. 
+import itertools
+
+puzzelInput = [int(x) for x in open("rawInput.txt").readlines()]
+print(sum(puzzelInput))
+
+workingNum = 0
+repeatedNum = {0}
+for num in itertools.cycle(puzzelInput):
+	workingNum += num
+	if workingNum in repeatedNum:
+		print(workingNum); break
+	repeatedNum.add(workingNum)
