8ffd752d16d3b085d7e0d816d0603a0397b0c4b5
Jayden Kerr <jayden@jaydenkerr.net>
2018-12-04 22:03:00 +1030

    Commencing Day 1

---
+++ Day 1/Day 1.1.py
@@ -0,0 +1,12 @@
+# Advent of Code 2018
+# Jayden Kerr
+# Day 1
+# Puzzle 1
+
+# Might come back to this - I need to save the session cookie once logged in order to cURL the contents of the page.
+#import requests
+
+#inputRequest = requests.get('https://adventofcode.com/2018/day/1/input')
+#print(inputRequest.text)
+
+puzzelInput = []
